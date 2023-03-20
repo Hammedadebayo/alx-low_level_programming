66fa28fa81dbb1ad2dac17f19d9410e3450d5ac3
hammedadebayo <hammedadebayo220@gmail.com>
2023-03-19 18:17:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unixstd.h>
 /**
  * main - C program that print a line to the standard error
- * Return: 1 (success)
+ * Return 1 (success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
