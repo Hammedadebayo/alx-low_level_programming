a07652fca5198ccb779f3a8a22e4b4e87c72d0d0
hammedadebayo <hammedadebayo220@gmail.com>
2023-03-20 17:40:46 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-        int n = 97;
+	int n = 97;
 	int m = 65;
 
-        while (n <= 122)
-        {
-                putchar(n);
+	while (n <= 122)
+	{
+		putchar(n);
 		n++;
-        }
+	}
 	while (m <= 90)
 	{
 		putchar(m);
 		m++;
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
