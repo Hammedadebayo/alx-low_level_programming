e5ce4ce21b77b3d3df26d6ec0d14448ec5ea798b
Hammedadebayo <hammedadebayo220@gmail.com>
2023-04-28 06:54:17 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- *  * print_list - prints list.
- *   * @h: param list
- *    * Return: struct
- *     */
+ *  print_list - prints list.
+ *  @h: param list
+ *  Return: struct
+ */
 size_t print_list(const list_t *h)
 {
 	size_t nodelen;
