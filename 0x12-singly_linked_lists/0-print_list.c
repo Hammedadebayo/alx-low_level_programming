66bfa044b08a205124c6d87fff8aa508dce13bd7
Hammedadebayo <hammedadebayo220@gmail.com>
2023-04-28 07:06:24 -0700

    Task 0 updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *  print_list - prints list.
- *  @h: param list
- *  Return: struct
+ * print_list - prints list.
+ * @h: param list
+ * Return: struct
  */
 size_t print_list(const list_t *h)
 {
@@ -12,9 +12,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		printf("[%d] %s\n", 0, "(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		nodelen++;
 	}
